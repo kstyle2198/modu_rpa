0caee4e5fae4b8a00332e2c6576467a73bb3a469
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2023-06-16 10:58:36 +0900

    apply try except syntex

--- rpa1.py
+++ rpa1.py
@@ -23,7 +23,8 @@ def web_wait(경로):
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, 경로)))
         # print("정상")
     except:
-        print("에러")
+        print("Web wait 함수 에러")
+        Slack_Msg("Web wait 함수 에러")
         
 df = pd.read_excel("data/전화번호리스트.xlsx")
 email = get_secret("email")
@@ -44,118 +45,126 @@ time.sleep(3)
 
 results = []
 
+
 for i in range(len(df['사번'])):
-    대상자 = df.iloc[i].values
-    print(대상자)
-    
-    # 문서함 초기화면 로딩
-    경로 = '//*[@id="root"]/div[2]/div/div[2]/div[2]/section[1]/ul/li[1]/a/div/span'
-    web_wait(경로)
-    nextXpath(경로).click()
-    time.sleep(0.1)
+    try:
+        대상자 = df.iloc[i].values
+        print(대상자)
+        
+        # 문서함 초기화면 로딩
+        경로 = '//*[@id="root"]/div[2]/div/div[2]/div[2]/section[1]/ul/li[1]/a/div/span'
+        web_wait(경로)
+        nextXpath(경로).click()
+        time.sleep(0.1)
 
-    #템플릿 화면 이동
-    경로 = '/html/body/div[1]/div[2]/div/div[2]/div[2]/section[1]/ul/li[3]/a/div/span'   
-    web_wait(경로)
-    nextXpath(경로).click()
-    time.sleep(0.1)
-    
-    # 첫번째 템플릿 선택
-    경로 = '//*[@id="root"]/div[2]/div/div[4]/div/div/div[2]/ul/div[1]/li/ul/li[1]/span'
-    web_wait(경로)
-    nextXpath(경로).click()
+        #템플릿 화면 이동
+        경로 = '/html/body/div[1]/div[2]/div/div[2]/div[2]/section[1]/ul/li[3]/a/div/span'   
+        web_wait(경로)
+        nextXpath(경로).click()
+        time.sleep(0.1)
+        
+        # 첫번째 템플릿 선택
+        경로 = '//*[@id="root"]/div[2]/div/div[4]/div/div/div[2]/ul/div[1]/li/ul/li[1]/span'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    #서명요청하기 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div[4]/div/div/div[2]/ul/div[1]/li/div[2]/div/ul/li[1]'
-    web_wait(경로)
-    nextXpath(경로).click()
-    
-    # 확인하고 시작하기 클릭
-    num = num_check(int(7+int(i)))
-    print(num)
-    경로 = f'/html/body/div[{num}]/div/div/div/div/div[2]/button[2]'
-    web_wait(경로)
-    nextXpath(경로).click()
+        #서명요청하기 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div[4]/div/div/div[2]/ul/div[1]/li/div[2]/div/ul/li[1]'
+        web_wait(경로)
+        nextXpath(경로).click()
+        
+        # 확인하고 시작하기 클릭
+        num = num_check(int(7+int(i)))
+        print(num)
+        경로 = f'/html/body/div[{num}]/div/div/div/div/div[2]/button[2]'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 이름 또는 회사명 입력
-    경로 = '//*[@id="first-participant"]/div[1]/div[2]/div/div/div/div/input'
-    web_wait(경로)
-    nextXpath(경로).click()  
-    nextXpath(경로).send_keys(str(대상자[1]))  
+        # 이름 또는 회사명 입력
+        경로 = '//*[@id="first-participant"]/div[1]/div[2]/div/div/div/div/input'
+        web_wait(경로)
+        nextXpath(경로).click()  
+        nextXpath(경로).send_keys(str(대상자[1]))  
 
-    # 카카오톡 전송버튼 클릭
-    경로 = '//*[@id="first-participant"]/div[1]/div[3]/button[2]'
-    web_wait(경로)
-    nextXpath(경로).click()
+        # 카카오톡 전송버튼 클릭
+        경로 = '//*[@id="first-participant"]/div[1]/div[3]/button[2]'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 전화번호 입력(뒤에 여덟자리만)
-    경로 = '//*[@id="first-participant"]/div[1]/div[3]/div[2]/div/input'
-    web_wait(경로)
-    nextXpath(경로).click()
-    nextXpath(경로).send_keys(str(대상자[2])) 
+        # 전화번호 입력(뒤에 여덟자리만)
+        경로 = '//*[@id="first-participant"]/div[1]/div[3]/div[2]/div/input'
+        web_wait(경로)
+        nextXpath(경로).click()
+        nextXpath(경로).send_keys(str(대상자[2])) 
 
-    #다음 단계로 버튼 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[3]/div/div/button/span'
-    web_wait(경로)
-    nextXpath(경로).click()
+        #다음 단계로 버튼 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[3]/div/div/button/span'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    #다음 단계로 버튼 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[4]/div/div/button/span'
-    web_wait(경로)
-    nextXpath(경로).click()
-    
-    # 입력설정 확인 (서명 없는 전자문서의 경우)
-    경로 = '/html/body/div[9]/div/div/div/div/div[2]/button[2]'
-    web_wait(경로)
-    nextXpath(경로).click()
-    
-    # 서명유효기간 설정 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/p/b'
-    web_wait(경로)
-    nextXpath(경로).click()
+        #다음 단계로 버튼 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[4]/div/div/button/span'
+        web_wait(경로)
+        nextXpath(경로).click()
+        
+        # 입력설정 확인 (서명 없는 전자문서의 경우만 아래 코드 블록 3줄 활성화)
+        경로 = '/html/body/div[9]/div/div/div/div/div[2]/button[2]'
+        web_wait(경로)
+        nextXpath(경로).click()
+        
+        # 서명유효기간 설정 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/p/b'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 서명유효기간 박스 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/input'
-    web_wait(경로)
-    nextXpath(경로).clear()
+        # 서명유효기간 박스 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/input'
+        web_wait(경로)
+        nextXpath(경로).clear()
 
-    # 서명유효기간 입력
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/input'
-    web_wait(경로)
-    nextXpath(경로).clear
-    nextXpath(경로).send_keys("4")   
+        # 서명유효기간 입력 (내일까지면 "1", 모레까지면 "2")
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/input'
+        web_wait(경로)
+        nextXpath(경로).clear()
+        nextXpath(경로).send_keys("4")   
 
-    # 설정완료 버튼 클릭
-    경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[3]/div/button[2]/span'
-    web_wait(경로)
-    nextXpath(경로).click()
+        # 설정완료 버튼 클릭
+        경로 = '//*[@id="root"]/div[2]/div/div/div/div/div[3]/div/button[2]/span'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 개인정보수집 동의
-    경로 = '//*[@id="agreement-check1"]'
-    web_wait(경로)
-    nextXpath(경로).click()
+        # 개인정보수집 동의
+        경로 = '//*[@id="agreement-check1"]'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 서명시 횟수차감 동의
-    경로 = '//*[@id="agreement-check2"]'
-    web_wait(경로)
-    nextXpath(경로).click()
+        # 서명시 횟수차감 동의
+        경로 = '//*[@id="agreement-check2"]'
+        web_wait(경로)
+        nextXpath(경로).click()
 
-    # 서명요청하기 클릭
-    경로 = '/html/body/div[11]/div/div/div/div/div[5]/div/button'
-    web_wait(경로)
-    nextXpath(경로).click()
-    
-    time.sleep(1)
+        # 서명요청하기 클릭
+        경로 = '/html/body/div[11]/div/div/div/div/div[5]/div/button'
+        web_wait(경로)
+        nextXpath(경로).click()
+        
+        time.sleep(1)
 
-    msg = f"{i}번째 {대상자[1]} 발송 완료"
-    print(msg)
-    results.append(msg)
-    
-    if (int(i)+1) % 50 == 0:
-        Slack_Msg(f"{int(i)+1}번째 발송 완료")
-        time.sleep(5)
+        msg = f"{i}번째 {대상자[1]} 발송 완료"
+        print(msg)
+        results.append(msg)
+        
+        if (int(i)+1) % 50 == 0:
+            Slack_Msg(f"{int(i)+1}번째 발송 완료")
+            time.sleep(5)
+            
+    except:
+        print(f"{int(i)+1}번째 {대상자[1]} 발송시 에러 발생")
+        Slack_Msg(f"{int(i)+1}번째 {대상자[1]} 발송시 에러 발생")
+        break
 
 print("전체 발송 완료")
 result_df = pd.DataFrame({"사번": 대상자[0] ,"결과": results})
 result_df.to_excel("./results1.xlsx")
 Slack_Msg(f"전체 {int(i)+1}명 발송 완료")
+
